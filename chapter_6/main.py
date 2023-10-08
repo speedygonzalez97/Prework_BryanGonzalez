146e8e0cb548d19ef7893ec13b3bbd86d71b2f3f
Bryan Gonzalez <gonzalez.bryan9@gmail.com>
2023-10-08 19:24:53 -0400

    changed to middle name

--- chapter_6/main.py
+++ chapter_6/main.py
@@ -1,7 +1,8 @@
 random_person= {'first_name': 'Bryan',
                 'last_name': 'Gonzalez',
                 'age': 26,
-                'city': ["astoria","oio","yuyyu"]}
+                'city': ["astoria","oio","yuyyu"],
+                'middle_name': 'Harry'}
 
 random_person.update({"age":28})
-print(random_person["age"])
+print(random_person["middle_name"])
